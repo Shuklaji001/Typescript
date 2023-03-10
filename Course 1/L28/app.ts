9722e88b50859012ee26d374d3dfa513bc4611ad
Shuklaji001 <Shukla@politicaldata.com>
2023-03-10 13:30:53 +0530

    TS tut

--- Course 1/L28/app.ts
+++ Course 1/L28/app.ts
@@ -1,7 +1,7 @@
-class Users{
-constructor(){
-    console.log("constructor called");
+class Users {
+  constructor(public name: string, public age: number, public email: string) {}
+  displayName() {
+    console.log(this.name, this.age, this.email);
+  }
 }
-}
-
-const u1 = new Users()
+const u1 = new Users("Ish", 22, "ish@gmail.com");
